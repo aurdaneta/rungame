055ad3371bd8fd24e71651d5a19b868b4b7c8413
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2015-05-15 19:55:06 -0430

    Agregando transicion de idle/walk a walk/jump

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -4,16 +4,23 @@ using System.Collections;
 public class Control : MonoBehaviour {
 
 	public Animator sintelAnim;
-	private float posX;
 
 	// Use this for initialization
 	void Start () {
-		sintelAnim = transform.GetComponent<Animator> ();
+		sintelAnim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		sintelAnim.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+
+		if (Input.GetButtonDown("Jump")){
+			sintelAnim.SetBool("Jumping", true);
+			Invoke("StopJumping", 1.0f);
+		}
+
+		/*
 		if (Input.GetKey (KeyCode.Space)) {
 			sintelAnim.CrossFade("Saltar", 0.5f);
 			//sintelAnim.Play("Saltar");
@@ -32,5 +39,10 @@ public class Control : MonoBehaviour {
 			posX += 0.2f;
 			transform.position = new Vector3(posX, transform.position.y, transform.position.z);
 		}
+		*/
+	}
+
+	void StopJumping(){
+		sintelAnim.SetBool("Jumping", false);
 	}
 }
